976240c9fb3e9506e6da942dc6edef87cc7dd7c7
B L <pandabl28@hotmail.com>
2018-05-13 07:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
-  return objectl;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key){
