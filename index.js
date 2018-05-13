ffdbd3e40e90c52e3faaa6e42b7b1e1340d05168
B L <pandabl28@hotmail.com>
2018-05-13 06:47:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes 
+
+function updateObjectWithKeyandValue(object, key, value){
+  return object.key = value;
+}
+
+function desctivelyUpdateObjectWithKeyandValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
+
+function desctivelyUpdateObjectWithKeyandValue(object, key)
